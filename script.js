07cb71db51eb49fb7ebe9d18c56603fa310b8d24
nanzim2 <renan.andrade2006@gmail.com>
2025-05-04 16:34:32 -0300

    validações: nome & idade

--- script.js
+++ script.js
@@ -2,12 +2,27 @@ const botao = document.getElementById("botao");
 
 botao.addEventListener("click", function(){
    var nome = prompt("Qual é o seu Nome?");
-   var idade = prompt("Qual a sua idade?");
-   window.alert(`Prazer em te conhecer ${nome}!`)
-   if (idade >= 18) {
-    window.alert(`Você tem ${idade} anos e você é MAIOR de idade`)
+
+   //validação nome
+   if (nome.trim() === "") {
+      alert("Por favor, digite um nome válido");
+      return; //sai da função
    }
-   else {
-    window.alert(`Você tem ${idade} anos e você é MENOR de idade`)
+
+   let idade = prompt("Qual a sua idade?");
+
+   //validação idade
+   if (!isNaN(idade) && idade.trim() !== "") { 
+      idade = parseInt(idade); //Converte para número
+      
+      window.alert(`Prazer em te conhecer ${nome}!`)
+
+      if (idade >= 18) {
+       window.alert(`Você tem ${idade} anos e você é MAIOR de idade`)
+      }  else {
+       window.alert(`Você tem ${idade} anos e você é MENOR de idade`)
+      }
+   } else {
+      alert ("Por favor, digite um número válido.")
    }
 });
